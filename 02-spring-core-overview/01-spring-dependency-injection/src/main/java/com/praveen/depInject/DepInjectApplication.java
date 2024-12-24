f09de6a8021ef4bf4d654212ead3bda1e66cc84e
praveenkumar09 <tpkprav@gmail.com>
2024-12-24 14:34:56 +0800

    Adding a example for constructor injection for students

---
+++ 02-spring-core-overview/01-spring-dependency-injection/src/main/java/com/praveen/depInject/DepInjectApplication.java
@@ -0,0 +1,13 @@
+package com.praveen.depInject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepInjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepInjectApplication.class, args);
+	}
+
+}
